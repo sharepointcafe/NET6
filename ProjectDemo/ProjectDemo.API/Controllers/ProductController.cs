dfa39f4867bb3d6065c8cbc5d9848d5a0f9ff68c
sharepointcafe <kundan.sriv@gmail.com>
2022-08-21 09:54:32 +0530

    new changes

---
+++ ProjectDemo/ProjectDemo.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectDemo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [Route("Check")]
+        public IActionResult CheckResponse()
+        {
+            //Write your code here
+            Response res = new Response()
+            {
+                ResponseId = Guid.NewGuid().ToString(),
+                StatusCode = "200",
+                Message =  "Service Executed Successfully"
+            };
+
+            return Ok(res);
+        }
+    }
+
+    public class Response
+    {
+        public string? ResponseId { get; set; }
+        public string? StatusCode { get; set; }            
+        public string?  Message { get; set; }
+    }
+}
